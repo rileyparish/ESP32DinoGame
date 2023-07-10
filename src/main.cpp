8639844df6a1d86c1544896786942f20467b75e9
Riley Parish <parish.riley@gmail.com>
2023-07-09 21:51:41 -0600

    Some tweaks; code is working as intended.

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,15 @@ Thanks, and have fun! :)
 #include <Arduino.h>
 #include <BleKeyboard.h>
 
+class DigitalInput {
+    public:
+        uint8_t digitalPin;  // the digital pin that is assigned to this finger     
+        bool isActive = false;      // whether electricity is flowing to the pin or not
+        uint8_t letter;
+        bool stateNeedsUpdate = false;   // stores whether the state of this input has changed from the previous iteration
+
+};
+
 class TouchInput {       
     public:             
         uint8_t capacitivePin;  // the GPIO pin that is assigned to this finger
@@ -18,39 +27,34 @@ class TouchInput {
         
 };
 
-class DigitalInput {
-    public:
-        uint8_t digitalPin;  // the digital pin that is assigned to this finger     
-        bool isActive = false;      // whether electricity is flowing to the pin or not
-        uint8_t letter;
-        bool stateNeedsUpdate = false;   // stores whether the state of this input has changed from the previous iteration
-
-};
 // this is the bluetooth keyboard
 BleKeyboard bleKeyboard;
 
 // since this script takes control of the keyboard, I want to be able to disable it. The start pin must read LOW before the ESP32 can control the keyboard.
 const int START_PIN = 0;       // BOOT button as labeled on the board
 
-// these are the physical GPIO pins on the ESP32
+// declare digital inputs (I only expect to use one, but this way the code can be easily adapted to add more)
+const int NUM_DIGITAL_INPUTS = 1;
+uint8_t digitalPins[NUM_DIGITAL_INPUTS] = {18};
+
+DigitalInput digitalInputs[NUM_DIGITAL_INPUTS];
+
+// declare touch inputs
 const int NUM_TOUCH_INPUTS = 1;
 uint8_t touchPins[NUM_TOUCH_INPUTS] = {T5};  // corresponds to pin D12 on board
-const int ACTIVE_THRESHOLD = 25; // Any reading below this value will register as a capacitive touch.
+const int ACTIVE_THRESHOLD = 9; // Any reading below this value will register as a capacitive touch. Neutral reading is 11, pressed is 6-7.
 
 // this list holds the information for each of the 5 finger inputs
 TouchInput touchInputs[NUM_TOUCH_INPUTS];
 
-
-const int NUM_DIGITAL_INPUTS = 1;
-uint8_t digitalPins[NUM_DIGITAL_INPUTS] = {33};
-
-DigitalInput digitalInputs[NUM_DIGITAL_INPUTS];
 
 unsigned long previousTime;
 
 void setup() {
     Serial.begin(115200);
     bleKeyboard.begin();
+
+    pinMode(BUILTIN_LED, OUTPUT);
     // don't proceed until I've confirmed that I want to give ESP32 control of the keyboard
     pinMode(START_PIN, INPUT_PULLUP);
     while(digitalRead(START_PIN) == HIGH){
@@ -65,7 +69,7 @@ void setup() {
         pinMode(digitalPins[i], INPUT_PULLUP);
     }
     for(int i = 0; i < NUM_DIGITAL_INPUTS; i++){
-        digitalInputs[i].digitalPin;
+        digitalInputs[i].digitalPin = digitalPins[i];
         digitalInputs[i].letter = KEY_UP_ARROW;
     }
 
@@ -97,7 +101,7 @@ void updateInputs(){
     }
 
     // update capacative inputs
-    for(int i = 0; i < NUM_TOUCH_INPUTS; i++){
+    for(int i = 0; i < NUM_TOUCH_INPUTS; i++){        
         if(touchInputs[i].isActive && touchRead(touchInputs[i].capacitivePin) > ACTIVE_THRESHOLD){
             // this means that a key is currently being pressed, but a touch is no longer being detected. The key presses are not in line with the state of the device and need an update
             touchInputs[i].isActive = false;
@@ -117,7 +121,8 @@ void updateKeystrokes(){
     for(int i = 0; i < NUM_DIGITAL_INPUTS; i++){
         // if the state of this input hasn't changed from the previous iteration, it doesn't need an update.
         if(digitalInputs[i].stateNeedsUpdate){
-            if(digitalInputs[i].isActive){
+            // the logic for jumping is inverted; we want to jump when the key is released, not when it's pressed
+            if(!digitalInputs[i].isActive){
                 bleKeyboard.press(digitalInputs[i].letter);
             }else{
                 bleKeyboard.release(digitalInputs[i].letter);
@@ -146,7 +151,10 @@ void updateKeystrokes(){
 void loop() {
     // no need to do anything if the keyboard is not currently connected to a device
     if(bleKeyboard.isConnected()){
+        digitalWrite(BUILTIN_LED, HIGH);
         updateInputs();
         updateKeystrokes();
-    }
+    }else{
+        digitalWrite(BUILTIN_LED, LOW);
+    }    
 }
